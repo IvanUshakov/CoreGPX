1a4811ee54c37876b26114fe370f77453a584d02
i.ushakov <ivan@ushakov.tech>
2024-02-04 22:25:58 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "CoreGPX",
     platforms: [
-        .iOS(.v11), .macOS(.v10_13), .watchOS(.v4)
+        .iOS(.v12), .macOS(.v10_13), .watchOS(.v4), .visionOS(.v1), .tvOS(.v12)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -29,6 +29,5 @@ let package = Package(
             name: "CoreGPXTests",
             dependencies: ["CoreGPX"],
             path: "Example/Tests"),
-        ],
-    swiftLanguageVersions: [ .v4_2, .v5 ]
+        ]
 )
